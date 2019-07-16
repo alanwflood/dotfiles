fc1e53d79190049775d4029862a947675a6fadc2
Alan Flood (Digital) <alan.flood@wipro.com>
2019-07-16 13:07:26 +0100

    Update setup script

--- .setup.sh
+++ .setup.sh
@@ -17,10 +17,12 @@ brew install imagemagick --with-webp
 brew install nmap
 brew install speedtest_cli
 brew install tmux
+brew install tree
 brew install reattach-to-user-namespace --with-wrap-pbcopy-and-pbpaste
 brew install wget --with-iri
 # Ranger + it's dependencies
-brew install ranger libcaca highlight atool lynx w3m elinks poppler transmission mediainfo exiftool
+# brew install ranger libcaca highlight atool lynx w3m elinks poppler transmission mediainfo exiftool
+brew install nnn
 
 # Development
 brew install python
@@ -30,14 +32,17 @@ brew install redis
 brew install mongodb && brew services start mongodb
 brew install rustup
 
+# Language Servers
+brew install ccls
+
 # Install rust language with sane defaults
 rustup-init -y
 rustup default stable
 rustup component add rls-preview rust-analysis rust-src
 
 # Install Dart
-brew tap dart-lang/dart
-brew install dart
+# brew tap dart-lang/dart
+# brew install dart
 
 # webfont generators
 brew tap bramstein/webfonttools
@@ -47,15 +52,9 @@ brew install woff2
 
 # the rest
 brew install dark-mode
-brew install buku
-echo 'MAKE SURE YOU LN TO BUKUS DB TO THE ONE ON DROPBOX'
 brew install git
 brew install git-lfs
-brew install joplin
-brew install mpd
-brew install ncmpcpp
 brew install ripgrep
-# brew install the_silver_searcher
 
 brew install fzf
 $(brew --prefix)/opt/fzf/install
@@ -64,6 +63,7 @@ brew install zplug
 brew install trash
 brew install youtube-dl
 
+# Neovim
 brew install neovim
 # Install vim plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
@@ -72,6 +72,7 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 pip2 install --user neovim
 pip3 install --user neovim
 
+# Emacs
 # brew tap railwaycat/emacsmacport
 # brew install emacs-mac --with-gnutls --with-imagemagick --with-modules --with-texinfo --with-xml2 --with-spacemacs-icon
 # brew linkapps emacs-mac
@@ -80,11 +81,9 @@ pip3 install --user neovim
 # version managers
 # nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-# yarn
-# brew install yarn
 # chruby
-# brew install chruby
-# brew install ruby-install
+brew install chruby
+brew install ruby-install
 
 # Remove outdated versions from the cellar.
 brew cleanup
@@ -93,42 +92,45 @@ brew cleanup
 brew tap caskroom/cask
 
 echo 'INSTALLING THROUGH BREW CASK'
+# old
+# brew cask install charles
+# brew cask install iterm2
+# brew cask install oni
+# brew cask install smcfancontrol
+# brew cask install transmission-remote-gui
+# brew cask install whatsapp
+
+brew cask install alcritty
 brew cask install alfred
 brew cask install android-studio android-platform-tools
 brew cask install bartender
 brew cask install bettertouchtool
 brew cask install bitwarden
 brew cask install calibre
-brew cask install charles
 brew cask install dbeaver-community
 brew cask install discord
 brew cask install disk-inventory-x
 brew cask install docker
 brew cask install dropbox
-brew cask install intellij-idea-ce
+brew cask install google-chrome
 brew cask install imageoptim
-brew cask install iterm2
+brew cask install intellij-idea-ce
 brew cask install java
-brew cask install joplin
 brew cask install mongodb-compass
 brew cask install mpv
 brew cask install nextcloud
 brew cask install ngrok
-brew cask install oni
 brew cask install onyx
 brew cask install osxfuse
 brew cask install plex-media-player
 brew cask install postman
 brew cask install sketch
-brew cask install smcfancontrol
 brew cask install soulseek
 brew cask install spotify
 brew cask install the-unarchiver
-brew cask install transmission-remote-gui
 brew cask install tunnelblick
 brew cask install veracrypt
 brew cask install virtualbox vagrant
-brew cask install whatsapp
 brew cask install zeplin
 
 # Fetch and install firefox nightly
@@ -140,7 +142,19 @@ brew tap buo/cask-upgrade
 
 echo 'INSTALLING THROUGH NPM'
 # global js packages
-npm i -g lighthouse prettier eslint js-beautify now preact-cli bs-platform @angular/cli vue-cli bs-platform javascript-typescript-langserver ocaml-language-server vue-language-server
+npm i -g lighthouse \
+        bs-platform \
+        eslint \
+        javascript-typescript-langserver \
+        js-beautify \
+        neovim \
+        now \
+        npm-check-updates \
+        preact-cli \
+        prettier \
+        tldr \
+        vue-cli \
+        vue-language-server
 
 echo 'INSTALLING THROUGH PIP'
 # global python packages
