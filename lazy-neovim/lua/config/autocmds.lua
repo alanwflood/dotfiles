42b65996dfcd3c0c207cf10cf2dcfff94776f8c5
Alan Flood <alan@Alans-MacBook-Pro.local>
2023-11-03 23:21:33 +0000

    lazyvim: move over missing commands

--- lazy-neovim/lua/config/autocmds.lua
+++ lazy-neovim/lua/config/autocmds.lua
@@ -1,10 +1,34 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
---
+local cmd = vim.api.nvim_create_user_command
+
 local has_neogit, neogit = pcall(require, "neogit")
 if has_neogit then
-  vim.api.nvim_create_user_command("G", function()
+  cmd("G", function()
     neogit.open({ kind = "auto" })
   end, { bang = true, desc = "Open Neogit" })
 end
+
+-- Handle my most incorrect commands
+local function handleCommand(command, vimCommand)
+  cmd(command, function()
+    vim.cmd(vimCommand)
+  end, {})
+end
+
+handleCommand("WQ", "wq")
+handleCommand("Wq", "wq")
+handleCommand("Wqa", "wqa")
+handleCommand("WQa", "wqa")
+handleCommand("W", "w")
+handleCommand("Q", "q")
+handleCommand("Qa", "qa")
+
+cmd("Light", function()
+  vim.opt.background = "light"
+end, { bang = true, desc = "set light theme" })
+
+cmd("Dark", function()
+  vim.opt.background = "dark"
+end, { bang = true, desc = "set dark theme" })
--- lazy-neovim/lua/config/options.lua
+++ lazy-neovim/lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+--Save undo history
+vim.opt.backupdir = os.getenv("HOME") .. "/.vim/backupdir"
+vim.opt.directory = os.getenv("HOME") .. "/.vim/swapdir"
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+-- I like having a separate clipboard for vim things
+vim.opt.clipboard = ""
+
+vim.opt.relativenumber = false
